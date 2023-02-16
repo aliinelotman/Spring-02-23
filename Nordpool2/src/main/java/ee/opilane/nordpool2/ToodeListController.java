c4a9aa0f69b6774699e8daafe946ba73d54a804a
Aliine Lotman <aliinelotman@gmail.com>
2023-02-16 15:33:13 +0200

    ToodeListControlleri lisad

--- Nordpool2/src/main/java/ee/opilane/nordpool2/ToodeListController.java
+++ Nordpool2/src/main/java/ee/opilane/nordpool2/ToodeListController.java
@@ -1,5 +1,7 @@
 package ee.opilane.nordpool2;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -11,7 +13,26 @@ public class ToodeListController {
     List<Toode> tooted = new ArrayList<>(Arrays.asList(
           new Toode(38993, "Juust", 8.0, true),
           new Toode(389944, "Sai", 8.0, false),
-          new Toode(389911, "Piim", 8.0, true);
+          new Toode(389911, "Piim", 8.0, true)
 
     ));
+
+    @GetMapping("kustuta/{index}")
+    private List<Toode> kustutaToode(@PathVariable int index) {
+        tooted.remove(index);
+        return tooted;
+    }
+
+    @GetMapping("vaata/{index}")
+    private Toode vaataToodet(@PathVariable int index){
+        return tooted.get(index);
+    }
+
+    @GetMapping("tuhjendaTooted")
+    private List<Toode> tuhjendaTooted(){
+        tooted.clear();
+        return tooted;
+    }
+
+
 }
